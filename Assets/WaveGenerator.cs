e23496662b80c813422426b739de5237cd1619a8
Thomas R. Koll <tomk32@tomk32.de>
2013-11-25 09:57:36 +0100

    Distribute waves more nicely

--- Assets/WaveGenerator.cs
+++ Assets/WaveGenerator.cs
@@ -12,25 +12,20 @@ public class WaveGenerator : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
     waveFrequency = 0.5f;
-    float verticalSize   = (float) Camera.main.orthographicSize * 2.0f;
-    float horizontalSize = (float) verticalSize * Screen.width / Screen.height;
-    for(int x = numberOfWaves / 20; x > 0; x--) {
-      for(int y = numberOfWaves / 40; y > 0; y--) {
-        GenerateWave(Random.Range(0, horizontalSize), verticalSize);
-      }
-    }
 	}
 
   void Update() {
     dtSinceLastWave += Time.deltaTime;
     if(dtSinceLastWave > waveFrequency) {
       dtSinceLastWave = 0;
-      GenerateWave(Random.Range(0, Screen.width), Screen.height);
+      GenerateWave();
     }
   }
 
-  void GenerateWave(float x, float y) {
-    Vector3 position = new Vector3(x, y, 0.0f);
+  void GenerateWave() {
+    float verticalSize   = (float) Camera.main.orthographicSize * 2.0f;
+    float horizontalSize = (float) verticalSize * Screen.width / Screen.height;
+    Vector3 position = new Vector3(Random.Range(0, horizontalSize), verticalSize, 0.0f);
     GameObject wave = (GameObject) Instantiate(waveTypes[0], position, Quaternion.identity);
     wave.transform.parent = transform;
   }
