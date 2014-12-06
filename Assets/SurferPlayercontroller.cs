02239200ea1eaa3df4feb4ee11267e642f66afc2
Thomas R. Koll <tomk32@tomk32.de>
2014-12-06 21:57:53 +0100

    Use Animations for the surfer to switch sprites between states

--- Assets/SurferPlayercontroller.cs
+++ Assets/SurferPlayercontroller.cs
@@ -4,14 +4,13 @@ using System.Collections;
 public class SurferPlayercontroller : MonoBehaviour {
   public Map map;
   private float speed;
-  public float[] speedFactors = new float[3] {1.0f, 0.25f, 0.025f};
+  public float[] speedFactors = new float[3] {1.0f, 0.25f, 0.05f};
   private float maxWalkSpeed;
   private float rotation;
   private float[] rotationFactors = new float[3] { 1.0f, 0.5f, 2.0f };
   private enum States:int { Walking, Paddling, Surfing };
   public Sprite[] stateSprites = new Sprite[3];
   public int state;
-  private float standingTime;
   private float last_speed;
 
 	// Use this for initialization
@@ -34,42 +33,37 @@ public class SurferPlayercontroller : MonoBehaviour {
     return speedFactors[state];
   }
 
+  void OnTriggerStay2D (Collider2D collider) {
+    //collision.gameObject.collider2D.enabled = false;
+    rigidbody2D.AddForce(collider.gameObject.GetComponent<WaveController>().ForceOnPlayer() * 10);
+    if (state == (int)States.Surfing) {
+      speed += Time.fixedDeltaTime;
+    }
+  }
+
 	// Update is called once per frame
 	void FixedUpdate () {
     float input_speed = Input.GetAxis("Vertical") * SpeedFactor();
+    Debug.Log(input_speed);
     if (last_speed > input_speed)
-      input_speed = 0.0f;
+      input_speed = 0;
     last_speed = input_speed;
-    rotation = Input.GetAxis("Horizontal") * RotationFactor();
 
     rigidbody2D.AddForce(gameObject.transform.up * input_speed);
 
+    rotation = Input.GetAxis("Horizontal") * RotationFactor();
     transform.Rotate(0, 0, -rotation * RotationFactor());
 
-    if (speed <= 0)
-      speed = 0;
-    else if (speed > maxWalkSpeed)
-      speed = maxWalkSpeed;
-    else if (speed > 0.0f)
-      // slow down
-      speed -= Time.deltaTime;
-
-
-    if(standingTime > 0) {
-      standingTime -= Time.deltaTime;
-    } else if(Input.GetButtonDown("Stand")) {
-      standingTime = Time.fixedDeltaTime * 40.0f;
+    if(Input.GetButtonDown("Stand")) {
       if(state == (int)States.Walking) {
+        GetComponent<Animator>().Play("Paddling");
         state = (int)States.Paddling;
       } else if (state == (int)States.Paddling) {
+        GetComponent<Animator>().Play("Surfing");
         state = (int)States.Surfing;
       } else if (state == (int)States.Surfing) {
         state = (int)States.Paddling;
       }
-      changeStanding();
     }
 	}
-  void changeStanding() {
-    gameObject.GetComponent<SpriteRenderer>().sprite = stateSprites[(int)state];
-  }
 }
