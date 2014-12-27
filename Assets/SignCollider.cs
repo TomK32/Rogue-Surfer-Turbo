8606e1975d2867df6a2be9e1af70e1eafe1f38af
Thomas R. Koll <tomk32@tomk32.de>
2014-12-27 10:57:46 +0100

    Add a little warning sign on the beach

---
+++ Assets/SignCollider.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SignCollider : MonoBehaviour {
+  public GameObject text;
+
+  void OnTriggerExit2D() {
+    text.SetActive(false);
+  }
+
+	void OnTriggerEnter2D() {
+    text.SetActive(true);
+	}
+}
